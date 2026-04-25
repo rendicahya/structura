7dc2c2bc626e3bba875e455b6dce413c8575f4de
Randy Cahya Wihandika <rendicahya@gmail.com>
2026-04-25 14:10:43 +0700

    docs: add JSDoc to formatters

--- src/lib/utils/formatters.js
+++ src/lib/utils/formatters.js
@@ -1,9 +1,17 @@
+/**
+ * @param {string|null|undefined} val
+ * @returns {string}
+ */
 export function formatValue(val) {
   if (!val) return 'null';
   if (/^-?\d+(\.\d+)?$/.test(val.trim())) return val.trim();
   return `"${val}"`;
 }
 
+/**
+ * @param {string|null|undefined} val
+ * @returns {string}
+ */
 export function formatPythonValue(val) {
   if (!val) return 'None';
   if (/^-?\d+(\.\d+)?$/.test(val.trim())) return val.trim();
